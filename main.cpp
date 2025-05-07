c30d0fa579b7090c9fae28afde83790df8a6f934
Ryan Dioneda <er.dioneda@gmail.com>
2025-05-07 19:36:33 -0400

    Implemented functionality to display overlay on defined key

--- main.cpp
+++ main.cpp
@@ -3,6 +3,42 @@
 
 // holds the handle to installed keyboard hook
 HHOOK g_hHook = nullptr;
+HWND g_hWndOverlay = nullptr;
+
+bool isCtrlHeld() {
+  return (GetAsyncKeyState(VK_LCONTROL) & 0x8000) ||
+         (GetAsyncKeyState(VK_RCONTROL) & 0x8000);
+}
+
+void updateOverlayVisibility() {
+  if (isCtrlHeld()) {
+
+    POINT pt;
+    if (GetCursorPos(&pt)) {
+
+      HMONITOR hMonitor = MonitorFromPoint(pt, MONITOR_DEFAULTTONEAREST);
+      MONITORINFO monitorInfo = {};
+      monitorInfo.cbSize = sizeof(monitorInfo);
+      if (GetMonitorInfo(hMonitor, &monitorInfo)) {
+        int width = 300;
+        int height = 150;
+        int x =
+            monitorInfo.rcWork.left +
+            (monitorInfo.rcWork.right - monitorInfo.rcWork.left - width) / 2;
+        int y =
+            monitorInfo.rcWork.top +
+            (monitorInfo.rcWork.bottom - monitorInfo.rcWork.top - height) / 2;
+
+        SetWindowPos(g_hWndOverlay, HWND_TOPMOST, x, y, width, height,
+                     SWP_NOACTIVATE | SWP_SHOWWINDOW);
+      }
+    }
+    ShowWindow(g_hWndOverlay, SW_SHOW);
+    UpdateWindow(g_hWndOverlay);
+  } else {
+    ShowWindow(g_hWndOverlay, SW_HIDE);
+  }
+}
 
 // hook callback
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
@@ -19,16 +55,8 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     auto kb = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam);
 
     bool isCtrlKey = kb->vkCode == VK_LCONTROL || kb->vkCode == VK_RCONTROL;
-    bool ctrlHeld = (GetAsyncKeyState(VK_LCONTROL) & 0x8000) ||
-                    (GetAsyncKeyState(VK_RCONTROL) & 0x8000);
-    bool isQPress =
+    bool isQKey =
         kb->vkCode == 'Q' && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN);
-
-    if (ctrlHeld && isQPress) {
-      OutputDebugStringA("[My Program] Ctrl + Q pressed. Exiting...\n");
-      PostQuitMessage(0);
-      return CallNextHookEx(g_hHook, nCode, wParam, lParam);
-    }
 
     // watch both left and right Ctrl
     // accessing kb.vkCode
@@ -37,21 +65,73 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
       case WM_KEYDOWN:
       case WM_SYSKEYDOWN:
         OutputDebugStringA("[HOOK] Ctrl DOWN\n");
+        updateOverlayVisibility();
         break;
       case WM_KEYUP:
       case WM_SYSKEYUP:
         OutputDebugStringA("[HOOK] Ctrl UP\n");
+        updateOverlayVisibility();
         break;
       }
     }
+
+    if (isCtrlHeld() && isQKey) {
+      OutputDebugStringA("[My Program] Ctrl + Q pressed. Exiting...\n");
+      PostQuitMessage(0);
+      return CallNextHookEx(g_hHook, nCode, wParam, lParam);
+    }
   }
+
   // pass hook to next chain
   return CallNextHookEx(g_hHook, nCode, wParam, lParam);
+}
+
+LRESULT CALLBACK OverlayWndProc(HWND hWnd, UINT msg, WPARAM wParam,
+                                LPARAM lParam) {
+  switch (msg) {
+  case WM_PAINT: {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+    SetBkMode(hdc, TRANSPARENT);
+    SetTextColor(hdc, RGB(255, 255, 255));
+    TextOutA(hdc, 50, 50, "Ctrl Held", 9);
+    EndPaint(hWnd, &ps);
+    return 0;
+  }
+  default:
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+  }
+}
+
+// window creation
+HWND CreateOverlayWindow(HINSTANCE hInst) {
+  const char CLASS_NAME[] = "OverlayWindowClass";
+
+  WNDCLASS wc = {};
+  wc.lpfnWndProc = OverlayWndProc;
+  wc.hInstance = hInst;
+  wc.lpszClassName = CLASS_NAME;
+  wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0)); // black
+
+  RegisterClass(&wc);
+
+  DWORD exStyle = WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST;
+  DWORD style = WS_POPUP;
+
+  HWND hWnd = CreateWindowEx(exStyle, CLASS_NAME, nullptr, style, 100, 100, 300,
+                             150, nullptr, nullptr, hInst, nullptr);
+
+  SetLayeredWindowAttributes(hWnd, 0, 180, LWA_ALPHA); // semi-transparent
+
+  return hWnd;
 }
 
 // program entry
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int) {
   OutputDebugStringA("Program starting...\n");
+
+  g_hWndOverlay = CreateOverlayWindow(hInst);
+  ShowWindow(g_hWndOverlay, SW_HIDE);
 
   // instal global hook
   g_hHook = SetWindowsHookExW(WH_KEYBOARD_LL,           // id hook
